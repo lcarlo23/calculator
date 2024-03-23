7a39ab721763131ba92656421667c60c4f1bdb8f
lcarlo23 <160224871+lcarlo23@users.noreply.github.com>
2024-03-23 11:48:07 +0100

    fix display behaviour after operator

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
-let num1 = 0;
+let num1;
 let op;
 let num2;
 
 
-function add(num1, num2) {    
+function add(num1, num2) {
+
     return num1 + num2;
+
 }
 
-const subtract = (num1, num2) => num1 - num2;
+function subtract(num1, num2) {
 
-const multiply = (num1, num2) => num1 * num2;
+    return num1 - num2;
 
-const divide = (num1, num2) => num1 / num2;
+}
 
-const operate = function (op) {
+function multiply(num1, num2) {
 
-    let total;
+    return num1 * num2;
+
+}
+
+function divide(num1, num2) {
+
+    return num1 / num2;
+
+}
+
+function operate(op) {
 
     if (op == '+') add();
     if (op == '-') subtract();
@@ -27,24 +39,58 @@ const operate = function (op) {
 const buttons = document.querySelector('#buttons');
 const display = document.querySelector('#display p');
 
-let dispVal = display.textContent;
+let dispNum = 0;
 
 buttons.addEventListener('mousedown', (e) => {
 
     if (e.target.tagName == 'BUTTON') e.target.classList.add('clicking');
 
 });
+
 buttons.addEventListener('mouseup', (e) => {
 
+    if (e.target.tagName != 'BUTTON') return;
+
     const target = e.target;
+    const btnText = target.textContent;
     const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    const operators = ['/', '*', '-', '+'];
+
 
     if (target.tagName == 'BUTTON') target.classList.remove('clicking');
 
-    if (target.textContent === 'CE') display.textContent = '0';
+    if (target.id === 'clear') {
 
-    if (numbers.includes(+target.textContent)) {
-        if (display.textContent == 0) display.textContent = '';
-        dispVal = display.textContent += target.textContent;
+        display.textContent = '0';
+
+        num1 = undefined;
+        num2 = undefined;
+        dispNum = [];
+
     };
+
+    if (numbers.includes(+btnText)) {
+        if (dispNum == 0) {
+            display.textContent = '';
+            dispNum = btnText;
+        } else {
+            dispNum += btnText;
+        };
+        display.textContent = dispNum;
+    };
+
+    if (operators.includes(btnText)) {
+        if (num1 == undefined) {
+            num1 = +dispNum;
+            display.textContent = num1;
+            dispNum = 0;
+        } else {
+            num2 = +dispNum;
+            display.textContent = operate(btnText);
+            
+        };
+    };
+
+    if (btnText == '=') operate(btnText);
+    console.log(num1, num2);
 });
